6dcca463accff08c55e53e2384a848a422a8e58f
VolTorian <voltorian.social@gmail.com>
2023-09-22 03:55:13 -0700

    function to update display from inputting numbers

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const display = document.getElementById("display");
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((button) => button.addEventListener("click", inputToDisplay));
+
 let firstNumber;
 let secondNumber;
 let operator;
+let textDisplay = "0";
 
 function add(num1, num2) {
     return (num1 + num2);
@@ -26,7 +31,13 @@ function operate(num1, num2, op) {
             return subtract(num1, num2);
         case "multiply":
             return multiply(num1, num2);
-        case "divide":
-            return divide(num1, num2);
+            case "divide":
+                return divide(num1, num2);
     }
+}
+
+function inputToDisplay() {
+    textDisplay += this.textContent;
+    display.textContent = parseInt(textDisplay);
+    // console.log(parseInt(textDisplay));
 }
