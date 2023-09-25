cb2c9d6a92b564a6f72bb161c5d25a002f59aa7c
VolTorian <voltorian.social@gmail.com>
2023-09-24 18:14:30 -0700

    clear/reset function, operation variables defaults

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const display = document.getElementById("display");
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach((button) => button.addEventListener("click", inputToDisplay));
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", reset);
 
-let firstNumber;
-let secondNumber;
+let firstNumber = 0;
+let secondNumber = 0;
 let operator;
 let textDisplay = "0";
 
@@ -31,12 +33,22 @@ function operate(num1, num2, op) {
             return subtract(num1, num2);
         case "multiply":
             return multiply(num1, num2);
-            case "divide":
-                return divide(num1, num2);
+        case "divide":
+            return divide(num1, num2);
     }
 }
 
+function reset() {
+    firstNumber = 0;
+    secondNumber = 0;
+    textDisplay = "0";
+    display.textContent = parseInt(textDisplay);
+}
+
 function inputToDisplay() {
+    if (textDisplay.length > 15) {
+        return;
+    }
     textDisplay += this.textContent;
     display.textContent = parseInt(textDisplay);
     // console.log(parseInt(textDisplay));
